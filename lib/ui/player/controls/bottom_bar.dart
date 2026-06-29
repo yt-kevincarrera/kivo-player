d913a5aacebd18cdff4bd79c6c49d2d02bcaaa99
Kevin Carrera <kevin.cc@selector.dev>
2026-06-29 16:04:15 -0400

    feat: aspect modes (fit/fill/stretch) cycled by the aspect button

--- lib/ui/player/controls/bottom_bar.dart
+++ lib/ui/player/controls/bottom_bar.dart
@@ -5,6 +5,7 @@ import '../../../core/settings/settings_provider.dart';
 import '../../../player/control/player_controller.dart';
 import '../speed/speed_panel.dart';
 import '../state/controls_visibility.dart';
+import '../state/aspect_state.dart';
 import '../state/lock_state.dart';
 import 'seek_bar.dart';
 
@@ -34,7 +35,11 @@ class BottomBar extends ConsumerWidget {
                 ref.read(controlsVisibleProvider.notifier).hide();
               },
             ),
-            IconButton(color: Colors.white38, icon: KivoIcon(KivoIcons.aspect, size: 24, opacity: 0.38), onPressed: null),
+            IconButton(
+              color: Colors.white,
+              icon: KivoIcon(KivoIcons.aspect, size: 24, color: Colors.white),
+              onPressed: () => ref.read(aspectModeProvider.notifier).cycle(),
+            ),
             IconButton(color: Colors.white38, icon: KivoIcon(KivoIcons.rotate, size: 24, opacity: 0.38), onPressed: null),
           ],
         ),
---
+++ lib/ui/player/state/aspect_state.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../core/settings/settings_provider.dart';
+
+enum AspectMode { fit, fill, stretch }
+
+BoxFit boxFitFor(AspectMode m) => switch (m) {
+      AspectMode.fit => BoxFit.contain,
+      AspectMode.fill => BoxFit.cover,
+      AspectMode.stretch => BoxFit.fill,
+    };
+
+AspectMode nextAspect(AspectMode m) =>
+    AspectMode.values[(m.index + 1) % AspectMode.values.length];
+
+AspectMode aspectFromSetting(String s) => switch (s) {
+      'fill' => AspectMode.fill,
+      'stretch' => AspectMode.stretch,
+      _ => AspectMode.fit,
+    };
+
+class AspectNotifier extends Notifier<AspectMode> {
+  @override
+  AspectMode build() => aspectFromSetting(ref.read(settingsProvider).defaultAspectMode);
+  void cycle() => state = nextAspect(state);
+}
+
+final aspectModeProvider =
+    NotifierProvider<AspectNotifier, AspectMode>(AspectNotifier.new);
